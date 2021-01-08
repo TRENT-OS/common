fc3b1a79923a0220039d154970551ceab4d02944
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-01-08 17:15:14 +0100

    [DEV] add function list_devices()

--- board_automation/wrapper_pyftdi.py
+++ board_automation/wrapper_pyftdi.py
@@ -12,18 +12,16 @@ from pyftdi import gpio, ftdi
 
 
 #-------------------------------------------------------------------------------
-def get_pyftdi_gpio(url):
-
-    # FTDI VID 0x0403
-    #
-    #   Device                                          | PID
-    # --------------------------------------------------+---------
-    #   FT232BM/L/Q, FT245BM/L/Q, FT232RL/Q, FT245RL/Q  | 0x6001
-    #   FT2232C/D/L, FT2232HL/Q                         | 0x6010
-    #   FT4232HL/Q                                      | 0x6011
-    #   FT232HL/Q                                       | 0x6014
-    #
-
+# FTDI VID 0x0403
+#
+#   Device                                          | PID
+# --------------------------------------------------+---------
+#   FT232BM/L/Q, FT245BM/L/Q, FT232RL/Q, FT245RL/Q  | 0x6001
+#   FT2232C/D/L, FT2232HL/Q                         | 0x6010
+#   FT4232HL/Q                                      | 0x6011
+#   FT232HL/Q                                       | 0x6014
+#
+def list_devices(vid = 0x0403):
     def get_id_from_file(dn, id_file):
         id_file_fqn = os.path.join(dn, id_file)
         if not os.path.exists(id_file_fqn): return None
@@ -31,14 +29,18 @@ def get_pyftdi_gpio(url):
 
     base_folder = '/sys/bus/usb/devices'
     for usb_path in sorted(os.listdir(base_folder)):
-         dn = os.path.join(base_folder, usb_path)
-         vid = get_id_from_file(dn, 'idVendor')
-         pid = get_id_from_file(dn, 'idProduct')
-         ser = get_id_from_file(dn, 'serial')
-         if (vid != '0403'): continue
-         print('{}:{} {:12} at {}'.format(vid, pid, ser or '', usb_path))
+        dn = os.path.join(base_folder, usb_path)
+        dev_vid = get_id_from_file(dn, 'idVendor')
+        dev_pid = get_id_from_file(dn, 'idProduct')
+        dev_ser = get_id_from_file(dn, 'serial')
+        if (dev_vid != '{:04x}'.format(vid)): continue
+        print('{}:{} {:12} at {}'.format(dev_vid, dev_pid, dev_ser or '', usb_path))
 
 
+#-------------------------------------------------------------------------------
+def get_pyftdi_gpio(url):
+
+    list_devices()
     ftdi.Ftdi.show_devices()
 
     print('opening {}'.format(url))
